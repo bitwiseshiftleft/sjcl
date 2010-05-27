1f84f6fcd0a1038b4d50cde5e26a80f00d740d9f
Mike Hamburg <hamburg@fas.harvard.edu>
2010-05-26 17:42:50 -0700

    initial version of site is up

---
+++ demo/example.js
@@ -0,0 +1,153 @@
+/* keep track of which salts have been used. */
+var form, usedIvs = {'':1}, usedSalts = {'':1};
+
+/* enter actions */
+var enterActions = {
+  password: doPbkdf2,
+  salt: doPbkdf2,
+  iter: doPbkdf2
+};
+
+function loaded() {
+  form = new formHandler('theForm', enterActions);
+  form._extendedKey = [];
+  sjcl.random.startCollectors();
+  document.getElementById("password").focus();
+}
+
+/* there's probaby a better way to tell the user something, but oh well */
+function error(x) {
+  alert(x);
+}
+
+/* compute PBKDF2 on the password. */
+function doPbkdf2(decrypting) {
+  var v = form.get(), salt=v.salt, key, hex = sjcl.codec.hex.fromBits, p={},
+      password = v.password;
+  
+  p.iter = v.iter;
+  
+  if (password.length == 0) {
+    if (decrypting) { error("Can't decrypt: need a password!"); }
+    return;
+  }
+  
+  if (salt.length === 0 && decrypting) {
+    error("Can't decrypt: need a salt for PBKDF2!");
+    return;
+  }
+  
+  if (decrypting || !v.freshsalt || !usedSalts[v.salt]) {
+    p.salt = v.salt;
+  }
+  
+  p = sjcl.misc.cachedPbkdf2(password, p);
+  form._extendedKey = p.key;
+  v.key = p.key.slice(0, v.keysize/32);
+  v.salt = p.salt;
+  
+  form.set(v);
+  form.plaintext.el.select();
+}
+/* Encrypt a message */
+function doEncrypt() {
+  var v = form.get(), iv = v.iv, password = v.password, key = v.key, adata = v.adata, aes, plaintext=v.plaintext, rp = {}, ct, p;
+  
+  if (plaintext === '' && v.ciphertext.length) { return; }
+  if (key.length == 0 && password.length == 0) {
+    error("need a password or key!");
+    return;
+  }
+  
+  p = { adata:v.adata,
+        iter:v.iter,
+        mode:v.mode,
+        ts:parseInt(v.tag),
+        ks:parseInt(v.keysize) };
+  if (!v.freshiv || !usedIvs[v.iv]) { iv:v.iv; }
+  if (!v.freshsalt || !usedSalts[v.salt]) { p.salt = v.salt; }
+  ct = sjcl.encrypt(password || key, plaintext, p, rp).replace(/,/g,",\n");
+
+  v.iv = rp.iv;
+  usedIvs[rp.iv] = 1;
+  if (rp.salt) {
+    v.salt = rp.salt;
+    usedSalts[rp.salt] = 1;
+  }
+  v.key = rp.key;
+  
+  if (v.json) {
+    v.ciphertext = ct;
+    v.adata = '';
+  } else {
+    v.ciphertext = ct.match(/ct:"([^"]*)"/)[1]; //"
+  }
+  
+  v.plaintext = '';
+  
+  form.set(v);
+  form.ciphertext.el.select();
+}
+
+/* Decrypt a message */
+function doDecrypt() {
+  var v = form.get(), iv = v.iv, key = v.key, adata = v.adata, aes, ciphertext=v.ciphertext, rp = {};
+  
+  if (ciphertext.length === 0) { return; }
+  if (!v.password && !v.key.length) {
+    error("Can't decrypt: need a password or key!"); return;
+  }
+  
+  if (ciphertext.match("{")) {
+    /* it's jsonized */
+    try {
+      v.plaintext = sjcl.decrypt(v.password || v.key, ciphertext, {}, rp);
+    } catch(e) {
+      error("Can't decrypt: "+e);
+      return;
+    }
+    v.mode = rp.mode;
+    v.iv = rp.iv;
+    v.adata = rp.adata;
+    if (v.password) {
+      v.salt = rp.salt;
+      v.iter = rp.iter;
+      v.keysize = rp.ks;
+      v.tag = rp.ts;
+    }
+    v.key = rp.key;
+    v.ciphertext = "";
+    document.getElementById('plaintext').select();
+  } else {
+    /* it's raw */
+    ciphertext = sjcl.codec.base64.toBits(ciphertext);
+    if (iv.length === 0) {
+      error("Can't decrypt: need an IV!"); return;
+    }
+    if (key.length === 0) {
+      if (v.password.length) {
+        doPbkdf2(true);
+        key = v.key;
+      }
+    }
+    aes = new sjcl.cipher.aes(key);
+    
+    try {
+      v.plaintext = sjcl.codec.utf8String.fromBits(sjcl.mode[v.mode].decrypt(aes, ciphertext, iv, v.adata, v.tag));
+      v.ciphertext = "";
+      document.getElementById('plaintext').select();
+    } catch (e) {
+      error("Can't decrypt: " + e);
+    }
+  }
+  form.set(v);
+}
+
+function extendKey(size) {
+  form.key.set(form._extendedKey.slice(0,size));
+}
+
+function randomize(field, words, paranoia) {
+  form[field].set(sjcl.random.randomWords(words, paranoia));
+  if (field == 'salt') { form.key.set([]); }
+}
---
+++ test/test.js
@@ -0,0 +1,134 @@
+sjcl.test = { vector: {}, all: {} };
+
+/* A bit of a hack.  Because sjcl.test will be reloaded several times
+ * for different variants of sjcl, but browserUtils will not, this
+ * variable keeps a permanent record of whether anything has failed.
+ */
+if (typeof browserUtil.allPassed === 'undefined') {
+    browserUtil.allPassed = true;
+}
+
+sjcl.test.TestCase = function(name, doRun) {
+  this.doRun = doRun;
+  this.name = name;
+  this.passes = 0;
+  this.failures = 0;
+  this.isUnimplemented = false;
+  sjcl.test.all[name] = this;
+};
+
+sjcl.test.TestCase.prototype = {
+  /** Pass some subtest of this test */
+  pass: function () { this.passes ++; },
+  
+  /** Fail some subtest of this test */
+  fail: function (message) {
+    if (message !== undefined) {
+      this.log("fail", "*** FAIL *** " + this.name + ": " + message);
+    } else {
+      this.log("fail", "*** FAIL *** " + this.name);
+    }
+    this.failures ++;
+    browserUtil.allPassed = false;
+  },
+  
+  unimplemented: function() {
+    this.isUnimplemented = true;
+  },
+  
+  /** Log a message to the console */
+  log: browserUtil.write,
+  
+  /** Require that the first argument is true; otherwise fail with the given message */
+  require: function (bool, message) {
+    if (bool) {
+      this.pass();
+    } else if (message !== undefined) {
+      this.fail(message);
+    } else {
+      this.fail("requirement failed");
+    }
+  },
+
+  /** Pause and then take the specified action. */
+  pauseAndThen: browserUtil.pauseAndThen,
+  
+  /** Continuation-passing-style iteration */
+  cpsIterate: browserUtil.cpsIterate,
+  
+  /** Continuation-passing-style iteration */
+  cpsMap: browserUtil.cpsMap,
+
+  /** Report the results of this test. */
+  report: function (repo) {
+    var t = (new Date()).valueOf() - this.startTime;
+    if (this.failures !== 0) {
+      repo.update("fail", "failed " + this.failures + " / " +
+                  (this.passes + this.failures) + " tests. (" + t + " ms)");
+    } else if (this.passes === 1) {
+      repo.update("pass", "passed. (" + t + " ms)");
+    } else if (this.isUnimplemented) {
+      repo.update("unimplemented", "unimplemented");
+    } else {
+      repo.update("pass", "passed all " + this.passes + " tests. (" + t + " ms)");
+    }
+    browserUtil.writeNewline();
+  },
+  
+
+  /** Run the test. */
+  run: function (ntests, i, cb) {
+    var thiz = this, repo = this.log("info", "Running " + this.name + "...");
+    this.startTime = (new Date()).valueOf();
+    this.pauseAndThen(function () {
+      thiz.doRun(function () {
+        thiz.report(repo);
+        cb && cb();
+      })
+    });
+  }
+};
+
+// pass a list of tests to run, or pass nothing and it will run them all
+sjcl.test.run = function (tests, callback) {
+  var t;
+    
+  if (tests === undefined || tests.length == 0) {
+    tests = [];
+    for (t in sjcl.test.all) {
+      if (sjcl.test.all.hasOwnProperty(t)) {
+        tests.push(t);
+      }
+    }
+  }
+  
+  browserUtil.cpsMap(function (t, i, n, cb) {
+    sjcl.test.all[tests[i]].run(n, i+1, cb);
+  }, tests, true, callback);
+};
+
+/* Several test scripts rely on sjcl.codec.hex to parse their test
+ * vectors, but we are not guaranteed that sjcl.codec.hex is
+ * implemented.
+ */
+sjcl.codec = sjcl.codec || {};
+sjcl.codec.hex = sjcl.codec.hex ||
+{
+  fromBits: function (arr) {
+    var out = "", i, x;
+    for (i=0; i<arr.length; i++) {
+      out += ((arr[i]|0)+0xF00000000000).toString(16).substr(4);
+    }
+    return out.substr(0, sjcl.bitArray.bitLength(arr)/4);//.replace(/(.{8})/g, "$1 ");
+  },
+  toBits: function (str) {
+    var i, out=[], len;
+    str = str.replace(/\s|0x/g, "");
+    len = str.length;
+    str = str + "00000000";
+    for (i=0; i<str.length; i+=8) {
+      out.push(parseInt(str.substr(i,8),16)^0);
+    }
+    return sjcl.bitArray.clamp(out, len*4);
+  }
+};
