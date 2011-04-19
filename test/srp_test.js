5fdfb69a7b773e37ab919970bc9877377183a40b
Quinn Slack <sqs@cs.stanford.edu>
2011-04-19 05:14:28 -0700

    test srp x and v

--- test/srp_test.js
+++ test/srp_test.js
@@ -5,15 +5,18 @@ new sjcl.test.TestCase("SRP known-answer (RFC 5054) tests", function (cb) {
     return;
   }
 
-  var i, kat = sjcl.test.vector.srp, tv, N, g, v;
+  var i, kat = sjcl.test.vector.srp, tv, N, g, v, x;
 
   for (i=0; i<kat.length; i++) {
     tv = kat[i];
-    N = sjcl.keyexchange.srp.knownGroups[tv.known_group_size].N;
-    g = sjcl.keyexchange.srp.knownGroups[tv.known_group_size].g;
-    v = sjcl.keyexchange.srp.makeVerifier(tv.I, tv.P, tv.s, N, g)
-      print("-> v = " + v);
-    this.require(sjcl.bitArray.equal(v, tv.v), "srpv #"+i);
+    N = sjcl.keyexchange.srp.knownGroup(tv.known_group_size).N;
+    g = sjcl.keyexchange.srp.knownGroup(tv.known_group_size).g;
+    tv.s = sjcl.codec.hex.toBits(tv.s);
+    x = sjcl.keyexchange.srp.makeX(tv.I, tv.P, tv.s);
+    this.require(sjcl.codec.hex.fromBits(x).toUpperCase() === tv.x, "srpx #"+i);
+
+    v = sjcl.keyexchange.srp.makeVerifier(tv.I, tv.P, tv.s, N, g);
+    this.require(v.equals(new sjcl.bn(tv.v)), "srpv #"+i);
   }
   cb && cb();
 });
