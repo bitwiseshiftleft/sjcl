de1d75cfbdc029d2bee347c5cb551ee2a33e84a7
Artem S Vybornov <vybornov@gmail.com>
2011-06-16 02:37:18 +0400

    RIPEMD160: Keys precomputation, endianess conversion, fix padding

--- test/ripemd160_test.js
+++ test/ripemd160_test.js
@@ -4,11 +4,12 @@ new sjcl.test.TestCase("RIPEMD-160", function (cb) {
     cb && cb();
     return;
   }
-  
+
   var i, kat=sjcl.test.vector.ripemd160, p=0, f=0;
   for (i=0; i<kat.length; i++) {
     var out = sjcl.hash.ripemd160.hash(kat[i][0]);
     this.require(sjcl.codec.hex.fromBits(out) == kat[i][1], i);
   }
+
   cb && cb();
 });
