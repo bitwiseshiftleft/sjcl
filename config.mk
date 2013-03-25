a682019a91b3446c24502a346933973266f8562c
Mike Hamburg <github@shiftleft.org>
2013-03-25 00:01:33 -0700

    pull changes across from ecc branch.

--- config.mk
+++ config.mk
@@ -1,2 +1,2 @@
-SOURCES= core/sjcl.js core/aes.js core/bitArray.js core/codecString.js core/codecHex.js core/codecBase64.js core/sha256.js core/ccm.js core/ocb2.js core/gcm.js core/hmac.js core/pbkdf2.js core/random.js core/convenience.js 
+SOURCES= core/sjcl.js core/aes.js core/bitArray.js core/codecString.js core/codecHex.js core/codecBase64.js core/codecBytes.js core/sha256.js core/sha512.js core/sha1.js core/ccm.js core/cbc.js core/ocb2.js core/gcm.js core/hmac.js core/pbkdf2.js core/random.js core/convenience.js core/bn.js core/ecc.js core/srp.js 
 COMPRESS= core_closure.js
