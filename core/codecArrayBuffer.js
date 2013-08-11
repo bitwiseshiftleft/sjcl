59419ff3b459f114595ead43c27d160870ae214c
Marco Munizaga <marco.munizaga@grooveshark.com>
2013-08-10 20:57:32 -0400

    Added faster arraybuffer version of ccm

--- core/codecArrayBuffer.js
+++ core/codecArrayBuffer.js
@@ -18,12 +18,32 @@ if (typeof(ArrayBuffer) === 'undefined') {
 /** @namespace ArrayBuffer */
 sjcl.codec.arrayBuffer = {
   /** Convert from a bitArray to an ArrayBuffer. */
-  fromBits: function (arr) {
-    var out, i;
-    out = new DataView(new ArrayBuffer(arr.length*4)) //each item in the array has a 32bit int (4Bytes of data). so we want a total of 4*arr.length of bytes
-    for (i=0; i<arr.length; i++) {
-      out.setUint32(i*4, (arr[i]<<32)) //get rid of the higher bits
+  fromBits: function (arr, padding, padding_count) {
+    var out, i, ol, tmp, smallest;
+    padding = padding==undefined  ? true : padding
+    padding_count = padding_count || 8
+    ol = sjcl.bitArray.bitLength(arr)/8
+    if (padding && ol%padding_count !== 0){
+      ol += padding_count - (ol%padding_count)
     }
+
+
+    //padded temp for easy copying
+    tmp = new DataView(new ArrayBuffer(arr.length*4)) 
+    for (i=0; i<arr.length; i++) {
+      tmp.setUint32(i*4, (arr[i]<<32)) //get rid of the higher bits
+    }
+
+    //now copy the final message if we are not going to 0 pad
+    
+    out = new DataView(new ArrayBuffer(ol))
+
+    smallest = tmp.byteLength < out.byteLength ? tmp.byteLength : out.byteLength
+    for(i=0; i<smallest; i++){
+      out.setUint8(i,tmp.getUint8(i))
+    }
+
+
     return out.buffer
   },
 
@@ -48,6 +68,10 @@ sjcl.codec.arrayBuffer = {
       for (var i = 0; i < stringBufferView.byteLength; i+=2) {
           if (i%16 == 0) string += ('\n'+(i).toString(16)+'\t')
           string += ( pad(stringBufferView.getUint16(i).toString(16),4) + ' ')
+      }
+
+      if ( typeof console === undefined ){
+        console = console || {log:function(){}} //fix for IE
       }
       console.log(string.toUpperCase())
   }
