ef8503b26756120a8a68c8384aa32758213b1120
Catatonic Prime <catatonicprime@gmail.com>
2014-09-12 05:29:38 -0400

    restoring th roit sjcl.js (i think?)

--- test/ecc_conv.js
+++ test/ecc_conv.js
@@ -7,9 +7,9 @@ new sjcl.test.TestCase("ECC convenience test", function (cb) {
   
   try {  
     var keys = sjcl.ecc.elGamal.generateKeys(192,0);
-    
+ 
     var ciphertext = sjcl.encrypt(keys.pub, "hello world");
-    var plaintext  = sjcl.decrypt(keys.sec, ciphertext);
+//    var plaintext  = sjcl.decrypt(keys.sec, ciphertext);
     
     this.require(plaintext == "hello world");
   } catch(e) {
