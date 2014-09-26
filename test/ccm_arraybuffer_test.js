221c6edd91b06660694f377d9b63d5a6f507437e
Nils Kenneweg <nk@ovt.me>
2014-09-26 18:14:50 +0200

    make tests work in nodejs.

--- test/ccm_arraybuffer_test.js
+++ test/ccm_arraybuffer_test.js
@@ -1,22 +1,15 @@
 //Run using phantomjs, since rhino doesn't support array buffers, and node has an issue with create dataviews with a bytelength of 0
-sjcl = require("../sjcl.js")
-
-sjcl = sjcl || {}
-sjcl.test = {}
-sjcl.test.vector = {}
-require("./ccm_vectors.js")
-
-console.log("Running CCM using ArrayBuffer tests")
-var start_time = +(new Date())
+console.log("Running CCM using ArrayBuffer tests");
+var start_time = +(new Date());
 
 //This ccm implementation is only defined for IV Lengths of 8 bytes
 var applicable_tests = sjcl.test.vector.ccm.filter(function(test){
-  return test.iv.length === 16 
-})
+  return test.iv.length === 16 ;
+});
 
 applicable_tests.map(function(tv, index){
   var len = 32 * tv.key.length,
-      h = sjcl.codec.hex
+      h = sjcl.codec.hex,
       aes = new sjcl.cipher.aes(h.toBits(tv.key)),
       iv = h.toBits(tv.iv),
       ad = h.toBits(tv.adata),
@@ -28,22 +21,21 @@ applicable_tests.map(function(tv, index){
 
   pass_e = sjcl.bitArray.equal(
     sjcl.arrayBuffer.ccm.compat_encrypt(aes, pt, iv, ad, tlen), ct
-  )
+  );
 
   pass_d = sjcl.bitArray.equal(
     sjcl.arrayBuffer.ccm.compat_decrypt(aes, ct, iv, ad, tlen), pt
-  )
+  );
 
   if (!(pass_e && pass_d)){
-    console.log("Failed at : ",len+"-ccm #", index, "Pass?:", pass_d)
+    console.log("Failed at : ",len+"-ccm #", index, "Pass?:", pass_d);
     phantom.exit(1);
   }
 
   return pass_e && pass_d;
 
-})
+});
 
-var total_time = parseInt(+(new Date())-start_time)
-console.log("  + passed all",applicable_tests.length,"tests. ("+ total_time, "ms)")
-phantom.exit();
+var total_time = parseInt(+(new Date())-start_time);
+console.log("  + passed all",applicable_tests.length,"tests. ("+ total_time, "ms)");
 
