792fa090b47230ee757198da82dd4d63b167a5d4
Ben Galliart <ben@steadfast.net>
2015-10-12 23:53:20 -0500

    Modified closure shell script to automatically update Google Closure Compiler (when the program unzip is available along with either wget or curl).  Also updated the COPYRIGHT document to reflect that neither Google Closure Compiler or YUI Compress are covered under the same license as SJCL.

--- compress/compress_with_closure.sh
+++ compress/compress_with_closure.sh
@@ -2,6 +2,31 @@
 
 DIR=`dirname $0`
 
+URL="http://dl.google.com/closure-compiler/compiler-latest.zip"
+FILE=`echo $URL | sed 's#.*/##'`
+unzip > /dev/null 2> /dev/null
+if [ $? -eq 0 ] ; then
+  wget -V > /dev/null 2> /dev/null
+  if [ $? -eq 0 ] ; then
+    pushd . > /dev/null
+    cd $DIR
+    wget -q -N $URL
+    popd > /dev/null
+  else
+    curl -V > /dev/null 2> /dev/null
+    if [ $? -eq 0 ] ; then
+      curl -s -z $DIR/$FILE -o $DIR/$FILE $URL
+    fi
+  fi
+  echo test $DIR/$FILE
+  if [ -s $DIR/$FILE ] ; then
+    pushd . > /dev/null
+    cd $DIR
+    unzip -o $FILE compiler.jar
+    popd > /dev/null
+  fi
+fi
+
 $DIR/remove_constants.pl $1 | $DIR/opacify.pl > ._tmpRC.js
 
 echo -n '"use strict";'
