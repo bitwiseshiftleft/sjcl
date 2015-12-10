bb039f8c2e6a7f224b75954775ee1985b0cd6a05
Torben <torben.letorbi@gmail.com>
2015-12-10 13:11:11 +0100

    Remove leading-zero stripping from big number normalization

--- core/bn.js
+++ core/bn.js
@@ -496,9 +496,6 @@ sjcl.bn.prototype = {
     if (carry === -1) {
       limbs[i-1] -= pv;
     }
-    while (limbs.length > 0 && limbs[limbs.length-1] === 0) {
-      limbs.pop();
-    }
     return this;
   },
 
