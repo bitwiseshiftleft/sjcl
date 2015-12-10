2e828715daa64fc4ea7be8fab7d341e64618206f
Torben <torben.letorbi@gmail.com>
2015-12-10 13:41:11 +0100

    Add leading-zero stripping option to big number toString method

--- core/bn.js
+++ core/bn.js
@@ -88,12 +88,15 @@ sjcl.bn.prototype = {
   /**
    * Convert to a hex string.
    */
-  toString: function() {
-    this.fullReduce();
-    var out="", i, s, l = this.limbs;
-    for (i=0; i < this.limbs.length; i++) {
+  toString: function(doTrim) {
+    var i, s, l, out = new sjcl.bn(this);
+    out.fullReduce();
+	doTrim && out.trim();
+    l = out.limbs;
+	out = "";
+    for (i=0; i < l.length; i++) {
       s = l[i].toString(16);
-      while (i < this.limbs.length - 1 && s.length < 6) {
+      while (i < l.length - 1 && s.length < 6) {
         s = "0" + s;
       }
       out = s + out;
