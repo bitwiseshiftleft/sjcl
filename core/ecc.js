98bd475518c63e7714aca8dd189b32263b81819c
Steve Thomas <Sc00bz@users.noreply.github.com>
2015-12-31 23:13:14 -0600

    Added points and functions for SPAKE2/PAKE2+

--- core/ecc.js
+++ core/ecc.js
@@ -29,8 +29,6 @@ sjcl.ecc.point = function(curve,x,y) {
   this.curve = curve;
 };
 
-
-
 sjcl.ecc.point.prototype = {
   toJac: function() {
     return new sjcl.ecc.pointJac(this.curve, this.x, this.y, new this.curve.field(1));
@@ -259,25 +257,89 @@ sjcl.ecc.pointJac.prototype = {
  * @param {bigInt} p The prime modulus.
  * @param {bigInt} r The prime order of the curve.
  * @param {bigInt} a The constant a in the equation of the curve y^2 = x^3 + ax + b (for NIST curves, a is always -3).
+ * @param {bigInt} b The constant b in the equation of the curve y^2 = x^3 + ax + b.
  * @param {bigInt} x The x coordinate of a base point of the curve.
  * @param {bigInt} y The y coordinate of a base point of the curve.
+ * @param {bigInt} mx The x coordinate of the M point of the curve for SPAKE2/PAKE2+.
+ * @param {bigInt} my The y coordinate of the M point of the curve for SPAKE2/PAKE2+.
+ * @param {bigInt} nx The x coordinate of the N point of the curve for SPAKE2/PAKE2+.
+ * @param {bigInt} ny The y coordinate of the N point of the curve for SPAKE2/PAKE2+.
  */
-sjcl.ecc.curve = function(Field, r, a, b, x, y) {
+sjcl.ecc.curve = function(Field, r, a, b, x, y, mx, my, nx, ny) {
   this.field = Field;
   this.r = new sjcl.bn(r);
   this.a = new Field(a);
   this.b = new Field(b);
   this.G = new sjcl.ecc.point(this, new Field(x), new Field(y));
+  if (mx && my && nx && ny) {
+    this.M = new sjcl.ecc.point(this, new Field(mx), new Field(my));
+    this.N = new sjcl.ecc.point(this, new Field(nx), new Field(ny));
+  }
 };
 
-sjcl.ecc.curve.prototype.fromBits = function (bits) {
-  var w = sjcl.bitArray, l = this.field.prototype.exponent + 7 & -8,
-      p = new sjcl.ecc.point(this, this.field.fromBits(w.bitSlice(bits, 0, l)),
-                             this.field.fromBits(w.bitSlice(bits, l, 2*l)));
-  if (!p.isValid()) {
-    throw new sjcl.exception.corrupt("not on the curve!");
+sjcl.ecc.curve.prototype = {
+  fromBits: function (bits) {
+    var w = sjcl.bitArray, l = this.field.prototype.exponent + 7 & -8,
+        p = new sjcl.ecc.point(this, this.field.fromBits(w.bitSlice(bits, 0, l)),
+                               this.field.fromBits(w.bitSlice(bits, l, 2*l)));
+    if (!p.isValid()) {
+      throw new sjcl.exception.corrupt("not on the curve!");
+    }
+    return p;
+  },
+
+  /**
+   * Creates a deterministic random point in constant time.
+   * Uses the simplified Shallue-Woestijne-Ulas algorithm (https://eprint.iacr.org/2009/340.pdf, page 16)
+   *
+   * Yes: c192, c256, c384, c521, k192, k256
+   * No:  c224, k224
+   *
+   * @param {bigInt} num A number 0 <= x < sjcl.ecc.curve.field.modulus.
+   * @return {sjcl.ecc.point} A deterministic random point.
+   */
+  deterministicRandomPoint: function (num) {
+    var u, c, x2, x3, h2, h3, p, y2, y3, pow, zero = new this.field(0), one = new this.field(1);
+
+    if (!this.canDeterministicRandomPoint()) {
+      throw("sjcl.ecc.curve.deterministicRandomPoint(): Curve can't create a deterministic random point in constant time");
+    }
+
+    u = new this.field(num);
+    // c = -u^2
+    c = zero.sub(u.square());
+    // X2 = -b / a * (1 + 1 / (c^2 + c))
+    x2 = zero.sub(this.b.mul(this.a.inverse())).mul(one.add(c.square().add(c).inverse()));
+    // X3 = c * X2
+    x3 = c.mul(x2);
+    // h2 = X2^3 + a * X2 + b
+    h2 = x2.square().mul(x2).add(this.a.mul(x2)).add(this.b);
+    // h3 = X3^3 + a * X3 + b
+    h3 = x3.square().mul(x3).add(this.a.mul(x3)).add(this.b);
+    // if (h2 is square)
+    pow = this.field.modulus.add(one).normalize().halveM().halveM();
+    y2 =          h2.power(pow);
+    y3 = zero.sub(h3.power(pow));
+    if (h2.equals(y2.square())) {
+      // (X2, h2 ^ ((q + 1) / 4))
+      p = new sjcl.ecc.point(this, x2, y2);
+    } else {
+      // (X3, -h3 ^ ((q + 1) / 4))
+      p = new sjcl.ecc.point(this, x3, y3);
+    }
+    return p;
+  },
+
+  /**
+   * Determines if the curve can create a deterministic random point in constant time.
+   * Yes: c192, c256, c384, c521, k192, k256
+   * No:  c224, k224
+   *
+   * @return {Bool} True if this curve can.
+   */
+  canDeterministicRandomPoint: function () {
+    return this.field.modulus.mod(4).equals(3);
   }
-  return p;
 };
 
 sjcl.ecc.curves = {
@@ -287,7 +349,9 @@ sjcl.ecc.curves = {
     -3,
     "0x64210519e59c80e70fa7e9ab72243049feb8deecc146b9b1",
     "0x188da80eb03090f67cbf20eb43a18800f4ff0afd82ff1012",
-    "0x07192b95ffc8da78631011ed6b24cdd573f977a11e794811"),
+    "0x07192b95ffc8da78631011ed6b24cdd573f977a11e794811",
+    "0x2","0x2df5fa08ab474e8f8f2ad5caca8264347d1fb30043214687",
+    "0x3","0x64fc66b7c0f932d5564fa514b3ba7858c8ee083f8c728022"),
 
   c224: new sjcl.ecc.curve(
     sjcl.bn.prime.p224,
@@ -295,7 +359,9 @@ sjcl.ecc.curves = {
     -3,
     "0xb4050a850c04b3abf54132565044b0b7d7bfd8ba270b39432355ffb4",
     "0xb70e0cbd6bb4bf7f321390b94a03c1d356c21122343280d6115c1d21",
-    "0xbd376388b5f723fb4c22dfe6cd4375a05a07476444d5819985007e34"),
+    "0xbd376388b5f723fb4c22dfe6cd4375a05a07476444d5819985007e34",
+    "0x3","0x7cac269c67bd55ea14efff4eadefe5e74978514af14c88fab46ec046",
+    "0x5","0x725c51c9ec2d0e1eb2dbc219f8d67054e02eda5b8431629fe0c0ec8b"),
 
   c256: new sjcl.ecc.curve(
     sjcl.bn.prime.p256,
@@ -303,7 +369,9 @@ sjcl.ecc.curves = {
     -3,
     "0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b",
     "0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296",
-    "0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5"),
+    "0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5",
+    "0x5","0x459243b9aa581806fe913bce99817ade11ca503c64d9a3c533415c083248fbcc",
+    "0x6","0x36b24c2c54250ac2466985e533720047dcd102b80fe7c0e9220d5128828223cb"),
 
   c384: new sjcl.ecc.curve(
     sjcl.bn.prime.p384,
@@ -311,15 +379,19 @@ sjcl.ecc.curves = {
     -3,
     "0xb3312fa7e23ee7e4988e056be3f82d19181d9c6efe8141120314088f5013875ac656398d8a2ed19d2a85c8edd3ec2aef",
     "0xaa87ca22be8b05378eb1c71ef320ad746e1d3b628ba79b9859f741e082542a385502f25dbf55296c3a545e3872760ab7",
-    "0x3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f"),
-    
+    "0x3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f",
+    "0x2","0x732152442fb6ee5c3e6ce1d920c059bc623563814d79042b903ce60f1d4487fccd450a86da03f3e6ed525d02017bfdb3",
+    "0x3","0x6660041b1c7984620e8d7fd7ccdb50cc3ba816da14d41a4d8affaba8488867f0ca5a24f8d42dd7e44b530a27dc5b58da"),
+
   c521: new sjcl.ecc.curve(
     sjcl.bn.prime.p521,
     "0x1FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA51868783BF2F966B7FCC0148F709A5D03BB5C9B8899C47AEBB6FB71E91386409",
     -3,
     "0x051953EB9618E1C9A1F929A21A0B68540EEA2DA725B99B315F3B8B489918EF109E156193951EC7E937B1652C0BD3BB1BF073573DF883D2C34F1EF451FD46B503F00",
     "0xC6858E06B70404E9CD9E3ECB662395B4429C648139053FB521F828AF606B4D3DBAA14B5E77EFE75928FE1DC127A2FFA8DE3348B3C1856A429BF97E7E31C2E5BD66",
-    "0x11839296A789A3BC0045C8A5FB42C7D1BD998F54449579B446817AFBD17273E662C97EE72995EF42640C550B9013FAD0761353C7086A272C24088BE94769FD16650"),
+    "0x11839296A789A3BC0045C8A5FB42C7D1BD998F54449579B446817AFBD17273E662C97EE72995EF42640C550B9013FAD0761353C7086A272C24088BE94769FD16650",
+    "0x2","0xd9254fdf800496acb33790b103c5ee9fac12832fe546c632225b0f7fce3da4574b1a879b623d722fa8fc34d5fc2a8731aad691a9a8bb8b554c95a051d6aa505acf",
+    "0x4","0x3eae97c9f1965bfeb023757973f48b400405b8b2741c47a8839cb83cfdee4aea8b729ca7a63034b4055e124dfb8bb8058b74b4aee5d42e5741ec130466549d7e27"),
 
   k192: new sjcl.ecc.curve(
     sjcl.bn.prime.p192k,
@@ -327,7 +399,9 @@ sjcl.ecc.curves = {
     0,
     3,
     "0xdb4ff10ec057e9ae26b07d0280b7f4341da5d1b1eae06c7d",
-    "0x9b2f2f6d9c5628a7844163d015be86344082aa88d95e2f9d"),
+    "0x9b2f2f6d9c5628a7844163d015be86344082aa88d95e2f9d",
+    "0x3","0xc13d09285ad3c2891f1318edf676acb26d65aaecc45fdbf",
+    "0x5","0x231585f0375e78018e41d7a0a1c1ab087a0851ebd58dc1f0"),
 
   k224: new sjcl.ecc.curve(
     sjcl.bn.prime.p224k,
@@ -335,7 +409,9 @@ sjcl.ecc.curves = {
     0,
     5,
     "0xa1455b334df099df30fc28a169a467e9e47075a90f7e650eb6b7a45c",
-    "0x7e089fed7fba344282cafbd6f7e319f7c0b0bd59e2ca4bdb556d61a5"),
+    "0x7e089fed7fba344282cafbd6f7e319f7c0b0bd59e2ca4bdb556d61a5",
+    "0x2","0x16c4805dc7abc9a9c59dd8fb4d9d379d3bd4f4a2e9beb766d4d999ab",
+    "0x4","0x7f7a7ae1abee58fb26ffec3765dd846b9fec0bd24fdeb8383ff49c9"),
 
   k256: new sjcl.ecc.curve(
     sjcl.bn.prime.p256k,
@@ -343,8 +419,9 @@ sjcl.ecc.curves = {
     0,
     7,
     "0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
-    "0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8")
-
+    "0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8",
+    "0x2","0x66fbe727b2ba09e09f5a98d70a5efce8424c5fa425bbda1c511f860657b8535e",
+    "0x3","0x2f233395c8b07a3834a0e59bda43944b5df378852e560ebc0f22877e9f49bb4b")
 };
 
 sjcl.ecc.curveName = function (curve) {
