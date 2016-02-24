adb635a2cec36452c4211af0b3f16128323de143
Alexander Slesarev <alex.slesarev@nudatasecurity.com>
2016-02-25 09:18:45 +1000

    --no-export option has been added.

---
+++ core/exports.js
@@ -0,0 +1,8 @@
+if(typeof module !== 'undefined' && module.exports){
+  module.exports = sjcl;
+}
+if (typeof define === "function") {
+    define([], function () {
+        return sjcl;
+    });
+}
--- core/sjcl.js
+++ core/sjcl.js
@@ -67,12 +67,3 @@ var sjcl = {
     }
   }
 };
-
-if(typeof module !== 'undefined' && module.exports){
-  module.exports = sjcl;
-}
-if (typeof define === "function") {
-    define([], function () {
-        return sjcl;
-    });
-}
